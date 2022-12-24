9ce8aa977983c2fd2ca07f3962aa031f1eec48ae
VladimirKlepikov <radishev95@gmail.com>
2022-12-24 16:40:05 +0700

    commit!

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,50 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
-import java.util.List;
-
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
-
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      User user1 = new User("Vladimir", "Klepikov", "klepikov@gmail.com");
+      User user2 = new User("Ilya", "Smirnov", "smirnov@mail.ru");
+      User user3 = new User("Aleksandra", "Romashkova", "romashkovaa@yandex.ru");
+      User user4 = new User("Kristina", "Zavialova", "zavialovaK@gmail.com");
+
+      Car car1 = new Car("ToyotaLandCruiser", 300);
+      Car car2 = new Car("MersedesBrabus", 700);
+      Car car3 = new Car("BMWX6", 150);
+      Car car4 = new Car("RangeRoverSport", 500);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
+      for (User user : userService.listUsers()) {
+         System.out.println("------------------------------------");
+         System.out.println(user + " " + user.getCar());
+         System.out.println("------------------------------------");
       }
+      System.out.println("------------------------------------");
+      System.out.println(userService.getUserWithCar("ToyotaLandCruiser", 300));
+      System.out.println("------------------------------------");
 
+
+      try {
+         User notExistUser = userService.getUserWithCar("LadaGranta", 2190);
+      } catch (Exception e) {
+         System.out.println("------------------------------------");
+         System.out.println("Пользователя с такой машиной не существует в данной таблице");
+         System.out.println("------------------------------------");
+      }
       context.close();
    }
 }
