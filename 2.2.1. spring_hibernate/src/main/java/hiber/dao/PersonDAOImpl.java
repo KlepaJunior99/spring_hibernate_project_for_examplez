56567aa6f357fc18850d633ade03b7b43dce1483
VladimirKlepikov <radishev95@gmail.com>
2022-12-29 23:52:14 +0700

    commit!

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/PersonDAOImpl.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/PersonDAOImpl.java
@@ -16,26 +16,31 @@ public class PersonDAOImpl implements PersonDAO {
     @PersistenceContext
     private EntityManager entityManager;
     @Transactional
+    @Override
     public List<Person> index() {
         return entityManager.createQuery("select p from Person p", Person.class).getResultList();
     }
 
     @Transactional
+    @Override
     public Person show(int id) {
         return entityManager.find(Person.class,id);
     }
 
     @Transactional
+    @Override
     public void save(Person person) {
         entityManager.persist(person);
     }
 
     @Transactional
+    @Override
     public void update(Person updatedPerson) {
         entityManager.merge(updatedPerson);
     }
 
     @Transactional
+    @Override
     public void delete(int id) {
         entityManager.remove(show(id));
     }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/PersonService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/PersonService.java
@@ -4,9 +4,9 @@ import hiber.model.Person;
 import java.util.List;
 
 public interface PersonService {
-    public List<Person> index() ;
-    public Person show(int id) ;
-    public void save(Person person) ;
-    public void update(Person updatedPerson);
-    public void delete(int id);
+    List<Person> index() ;
+    Person show(int id) ;
+    void save(Person person) ;
+    void update(Person updatedPerson);
+    void delete(int id);
 }
